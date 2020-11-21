f4b92441943b3ba77a82f401d89b89e8fab0162e
Dilya Joseph <dilyajoseph@gmail.com>
2020-11-20 19:51:19 -0600

    MVP complete

--- app.js
+++ app.js
@@ -15,22 +15,12 @@ Truth or Drink
 // 4. Create game card modal
 // - have modal pop up once "start game" button is clicked
 // 5. Create countdown timer
-// 6.
+// - have timer restart evey time "next" is clicked
+// 6. Have player points counter
+// - add 20 points when "truth" is clicked
+// - +10 when "drink" button is clicked
 
 
-const questions = [
-    "Who would die first in a zombie appocalypse?",
-    "How often do you wash your bedsheets?",
-    "Who would die first in a zombie appocalypse?",
-    "Who do you think has the worst hygiene?"
-]
-
-let points = 0;
-let counter = 0;
-
-for (let i = 0; i < questions.length; i++) {
-
-}
 
 ///////////////////////////////////////////////////////////////////////////////
 // Cached DOM nodes
@@ -43,15 +33,16 @@ const modalInstructions = document.querySelector('.modal-instructions');
 const startGame = document.querySelector('.start-game');
 const gameContainer = document.querySelector('.game-container');
 const modalInnerText = document.querySelector('.modal--inner');
+const truth = document.getElementById("truth");
+const drink = document.getElementById("drink");
+const score = document.getElementById("score");
 const nextQ = document.querySelector('.next-q');
-const timeLeftDisplay = document.querySelector('#time-left')
+const player1Points = document.getElementById('player1Points');
+const player2Points = document.getElementById('player2Points');
+const timeLeftDisplay = document.querySelector('#time-left');
 
 
 
-
-
-
-// clear interval vs set interval
 
 ///////////////////////////////////////////////////////////////////////////////
 // Create Players
@@ -69,7 +60,10 @@ class Player {
 ///////////////////////////////////////////////////////////////////////////////
 const firstPlayer = new Player("Player 1", 0);
 const secondPlayer = new Player("Player 2", 0);
+let currentPlayer = 1;
 
+let points = 0;
+let counter = 0;
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -97,34 +91,138 @@ const openGameContainer = () => {
 const updateModalInnerText = () => {
     modalInnerText.innerHTML = questions[counter];
     counter++;
+    nextQ.classList.add('hide');
+}
+    
+
+
+
+// add points to players    
+const truthPoints = () => {
+    nextQ.classList.remove('hide');
+    if (currentPlayer === 1) {
+        firstPlayer.points+= 20;
+        currentPlayer = 2; 
     }
+    else {
+        secondPlayer.points+= 20;
+        currentPlayer = 1;
+    }
+    endGame();
+    updateScoreInDOM();
+}
+const drinkPoints = () => {
+    nextQ.classList.remove('hide');
+
+    if (currentPlayer === 1) {
+        firstPlayer.points+= 10;
+        currentPlayer = 2; 
+    }
+    else {
+        secondPlayer.points+= 10;
+        currentPlayer = 1;
+    }
+    endGame();
+    updateScoreInDOM();
+}
+
+const updateScoreInDOM = () => {
+    player1Points.innerHTML = firstPlayer.points;
+    player2Points.innerHTML = secondPlayer.points;
+}
+
+// let the questions continue for 10 rounds
+// unless a player reaches 50 points first 
+function endGame() {
+    if (firstPlayer.points >= 50 || secondPlayer.points >= 50) {
+        if (firstPlayer.points > secondPlayer.points) {
+            // need to display which player wins on screen
+            modalInnerText.innerHTML = ("You made it to 50 points! You win!")
+            // return win;
+        } else if (secondPlayer.points > firstPlayer.points) {
+            modalInnerText.innerHTML = ("You made it to 50 points! You win!")
+            // return win; 
+        }
+    //remove truth/drink/next
+
+    }
+}
 
 
+// create counter 
+// check win 
+const game = () => {
+    if(firstPlayer.score >= 50 || secondPlayer.score >= 50) {
+    endGame();
+    }
+}
+
+let myTimer;
+let currentTime = 10;
+const timer = () => {
+    if(currentTime <= 1) {
+        clearInterval(myTimer);
+    } 
+    currentTime--;
+    timeLeftDisplay.innerHTML = currentTime;
+}
+const stopTimer = () => {
+    clearInterval(myTimer)
+    currentTime = 10;
+    timeLeftDisplay.innerHTML = currentTime;
+    countDown();
+}
+const countDown = () => {
+    myTimer = setInterval(timer, 1000)
+}
 document.addEventListener('DOMContentLoaded', () => {
-
-    function countDown() {
-        // console.log("hello world")
-        let timeLeft = 10;
-        setInterval(function(){
-            if(timeLeft <= 0 ) {
-                clearInterval(timeLeft = 0)
-            }
-            timeLeftDisplay.innerHTML = timeLeft;
-            timeLeft -=1
-        },   1000)
-    }
+    game();
+    // function countDown() {
+    //     let timeLeft = 10;
+    //     setInterval(function(){
+    //         if(timeLeft <= 0 ) {
+    //             clearInterval(timeLeft = 0)
+    //         }
+    //         timeLeftDisplay.innerHTML = timeLeft;
+    //         timeLeft -=1
+    //     },   1000)
+    // }
     startGame.addEventListener('click', countDown)
-    nextQ.addEventListener('click', countDown)
+    nextQ.addEventListener('click', stopTimer)
     });
 
 
 
+const questions = [
+    "Who would die first in a zombie appocalypse?",
+    "How often do you wash your bedsheets?",
+    "Who would die first in a zombie appocalypse?",
+    "Who do you think has the worst hygiene?",
+    "Who would die first in a zombie appocalypse?",
+    "How often do you wash your bedsheets?",
+    "Who would die first in a zombie appocalypse?",
+    "Who do you think has the worst hygiene?",
+    "Who would die first in a zombie appocalypse?",
+    "How often do you wash your bedsheets?",
+  
+]
 
+
+// if player clicks on "Truth" button, player +20 points and pop up "next button"
+// if "drink", +10
+// pop up "next" button
+
+
+if (document.getElementById('truth').clicked === true) {
+    document.getElementById('score') = score + 20;
+}
+else if (document.getElementById('drink').clicked === true) {
+    document.getElementById('score') = score + 10;
+}
 
 
 
     
-// click on next = next question
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -134,6 +232,8 @@ document.addEventListener('DOMContentLoaded', () => {
 ratedEveryone.addEventListener("click", openModalInstructions);
 twentyOne.addEventListener("click", openModalInstructions);
 startGame.addEventListener("click", closeModalInstructions);
+truth.addEventListener("click", truthPoints);
+drink.addEventListener("click", drinkPoints);
 nextQ.addEventListener('click', updateModalInnerText);
 
 
@@ -144,20 +244,5 @@ window.onload = toggleModal;
 // Game board
 ///////////////////////////////////////////////////////////////////////////////
 
-// pop up game card, timer, player stats
-// 1st question is asked, timer begins countdown
-// When player clicks "truth" button player's points increase by 10
-// if "drink" player's points stay the same
-// when time is up, "next round" buttons pops up
-// when click on "next round", next round begins
 
 
-// text on card change when "next" is clicked 
-
-
-
-
-
-// function round() 
-// if round < 10
-// open modal, stop @ 10
